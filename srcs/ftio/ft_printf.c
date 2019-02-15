f7658285d79623674a4c026b5aeaceecd5ac6674
Arthur Baurens <elementscitiessuport@gmail.com>
2019-02-15 19:09:55 +0100

    removed a useless (for now) call into ft_printf

--- srcs/ftio/ft_printf.c
+++ srcs/ftio/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: abaurens <abaurens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 21:56:12 by abaurens          #+#    #+#             */
-/*   Updated: 2019/02/15 18:57:24 by abaurens         ###   ########.fr       */
+/*   Updated: 2019/02/15 19:09:24 by abaurens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,6 @@ int				ft_vprintf(const char *format, va_list ap)
 	ft_bzero(&data, sizeof(data));
 	data.err = get_errno();
 	data.use_chain_format = MAYBE;
-	if (!(data.vals = ft_memalloc(sizeof(t_arg) * (count_args(format) + 1))))
-		return (-1);
 	va_copy(data.va_lst, ap);
 	if ((size = get_non_arg(format, &data)) < 0)
 		return (ft_freturn(data.buf, ERROR));
