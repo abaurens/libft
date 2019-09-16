1393ea6006d439ea79dea0091400521cebabd101
Arthur Baurens <elementscitiessuport@gmail.com>
2019-09-16 16:46:17 +0200

    fixed a vidsual bug in the nfa representation of the regex when unifying expression with + quantifier inside

--- libs/ftregex/srcs/nfa/operations/nfa_union.c
+++ libs/ftregex/srcs/nfa/operations/nfa_union.c
@@ -6,7 +6,7 @@
 /*   By: abaurens <abaurens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 22:11:36 by abaurens          #+#    #+#             */
-/*   Updated: 2019/08/14 22:43:21 by abaurens         ###   ########.fr       */
+/*   Updated: 2019/09/16 16:42:00 by abaurens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,17 @@ t_nfa	*nfa_union(t_nfa *first, t_nfa *second)
 	t_nfa	*res;
 	char	*n1;
 	char	*n2;
+	int		i;
+	int		j;
 
 	if (!(res = new_nfa()))
 		return (NULL);
 	n1 = first->name + (*first->name == '(');
+	if ((i = ((*first->name == '(') && n1[(ft_idxof(')', n1) + 1)] == '(')))
+		n1--;
 	n2 = second->name + (*second->name == '(');
+	if ((j = ((*second->name == '(') && n2[(ft_idxof(')', n2) + 1)] == '(')))
+		n2--;
 	add_epsilon(res->start, first->start);
 	add_epsilon(res->start, second->start);
 	add_epsilon(first->end, res->end);
@@ -40,9 +46,9 @@ t_nfa	*nfa_union(t_nfa *first, t_nfa *second)
 	second->end->is_end = 0;
 	first->end->is_end = 0;
 	res->name[0] = '(';
-	ft_strncat(res->name, n1, ft_idxof(')', n1));
+	ft_strncat(res->name, n1, ft_idxof(i ? 0 : ')', n1));
 	ft_strcat(res->name, "|");
-	ft_strncat(res->name, n2, ft_idxof(')', n2));
+	ft_strncat(res->name, n2, ft_idxof(j ? 0 : ')', n2));
 	ft_strcat(res->name, ")");
 	return (res);
 }
