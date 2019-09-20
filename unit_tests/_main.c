bcdcb695b7661e310a4ff851e7d35f2fac08ba8b
Arthur Baurens <elementscitiessuport@gmail.com>
2019-09-20 08:44:30 +0200

    [unit tests] temp changement in the _main to test the new modifications of the printf function

--- unit_tests/_main.c
+++ unit_tests/_main.c
@@ -6,13 +6,13 @@
 /*   By: abaurens <abaurens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 21:17:31 by abaurens          #+#    #+#             */
-/*   Updated: 2019/09/20 08:24:00 by abaurens         ###   ########.fr       */
+/*   Updated: 2019/09/20 08:43:43 by abaurens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
+
 #define FT_DISABLE_WARNINGS
-*/
+
 #include <locale.h>
 #include <stdio.h>
 #include <errno.h>
@@ -24,9 +24,9 @@ void	test_vector_all(void);
 int		main(void)
 {
 	/*test_vector_all();*/
-	/*setlocale(LC_ALL, "");*/
 	errno = -15;
-	ft_printf("test: %d, %s, %c, %f, %x, %+e, %C, %m\n", 42, "test string a bit long", 'C', 42.42, 0x42, -52.84e-17, L'a');
-	   printf("test: %d, %s, %c, %f, %x, %+e, %C, %m\n", 42, "test string a bit long", 'C', 42.42, 0x42, -52.84e-17, L'a');
+	setlocale(LC_CTYPE, "");
+	ft_printf("test: %d, %s, %c, %f, %x, %+e, %C, %m | %R, %#R\n", 42, "test string a bit long", 'C', 42.42, 0x42, -52.84e-17, L'💜', '\033', '\033');
+	   printf("test: %d, %s, %c, %f, %x, %+e, %C, %m\n", 42, "test string a bit long", 'C', 42.42, 0x42, -52.84e-17, L'💜');
 	return (0);
 }
